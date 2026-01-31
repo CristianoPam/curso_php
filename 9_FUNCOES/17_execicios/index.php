22ed8e34c2e0590550ae41569a1652d1d0132028
CristianoPam <cpa.cristiano@gmail.com>
2026-01-30 20:39:28 -0400

    30-01-2026-2

--- 9_FUNCOES/17_execicios/index.php
+++ 9_FUNCOES/17_execicios/index.php
@@ -1,17 +1,26 @@
 <?php
 
-$qtdVogais=0;
+global $qtdVogais;
 
 function countVowels($a) {
-
-foreach ($a as $a) {
-
+    global $qtdVogais;
+    $qtdVogais = 0;
+    foreach (str_split($a) as $a) {
         switch ($a) {
-            case 'a':                
-            case 'e':                
+            case 'a':
+                $qtdVogais++;
+                break;               
+            case 'e': 
+                $qtdVogais++;
+                break;                    
             case 'i':
+                $qtdVogais++;
+                break;
             case 'o':
+                $qtdVogais++;
+                break;
             case 'u':
+                $qtdVogais++;
                 break;
             
             default:
@@ -26,3 +35,7 @@ foreach ($a as $a) {
 
 
 echo countVowels('girassol');
+echo "<br>";
+echo countVowels('Paraguai');
+echo "<br>";
+echo countVowels('abacaxi');
