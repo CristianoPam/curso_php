30c809f344e979a779d96c59ecb4fabc529ea67b
CristianoPam <100892314+CristianoPam@users.noreply.github.com>
2026-02-08 19:52:06 -0400

    08-02-2026

---
+++ 11_ARRAY_AVANCADO/01_adicionado_a_array/index.php
@@ -0,0 +1,52 @@
+<?php
+
+$arr = [];
+print_r($arr);
+echo "<br>";
+
+//adicioando valores
+$arr[0] = 10;
+print_r($arr); 
+echo "<br>";
+
+
+$arr[1]=15;
+print_r($arr);
+echo "<br>";
+
+
+$arr[5]= 20;
+print_r($arr);
+echo "<br>";
+
+//modificando valores
+
+$arr[1] += 55;
+
+print_r($arr);
+
+//array associativo
+
+$arrAssoc=[];
+echo "<br>";
+
+$arrAssoc["Carro"]= "BMW";
+echo "<br>";
+
+print_r($arrAssoc);
+echo "<br>";
+
+$arrAssoc["Aviao"] = "Boing";
+
+print_r($arrAssoc);
+
+$arrAssoc["Carro"]= "Ferrari";
+echo "<br>";
+
+print_r($arrAssoc);
+echo "<br>";
+
+
+
+
+
